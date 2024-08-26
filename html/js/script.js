00ab3647d311e565f19fcdb71ec5bb36be06d244
imran <imran1252004@gmail.com>
2024-08-26 10:17:44 +0530

    adding projects

---
+++ html/js/script.js
@@ -0,0 +1,43 @@
+let name='imran';
+var name2='immu';
+name2='IMMU'
+const val='40';
+//val='20'
+
+
+console.log(name, name2, val);
+console.log(name2);
+
+{
+    var num='30';
+    console.log(num);
+}
+console.log(num);
+
+let x=5;
+let y=x++;
+y=++x;
+console.log(y);
+
+//funxction
+function area(l,b){
+    return l*b
+}
+let ans=area(2,5);
+console.log(ans); //type 1
+
+function area(l,b){
+    return l*b
+}
+console.log(area(2,5)); //type 2
+
+/*let factor='Vijay';
+let fplayer='Dhoni';
+let fmovie='Leo';
+function favourite(){
+
+}*/
+
+for (let i=0; i<100; i=i+1){
+    console.log(i);
+}
